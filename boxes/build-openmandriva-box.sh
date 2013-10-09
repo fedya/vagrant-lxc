4296a39f8ce527e2967cb75b6b373eb623637614
Vokhmin Alexey V <avokhmin@gmail.com>
2013-10-09 20:09:04 +0400

    fixed ROOTFS

--- boxes/build-openmandriva-box.sh
+++ boxes/build-openmandriva-box.sh
@@ -26,7 +26,7 @@ ARCH=${2:-"x86_64"}
 PKG=vagrant-lxc-${RELEASE}-${ARCH}-${TODAY}.box
 WORKING_DIR=/tmp/vagrant-lxc-${RELEASE}
 VAGRANT_KEY="ssh-rsa AAAAB3NzaC1yc2EAAAABIwAAAQEA6NF8iallvQVp22WDkTkyrtvp9eWW6A8YVr+kz4TjGYe7gHzIw+niNltGEFHzD8+v1I2YJ6oXevct1YeS0o9HZyN1Q9qgCgzUFtdOKLv6IedplqoPkcmF0aYet2PkEDo3MlTBckFXPITAMzF8dJSIFo9D8HfdOV0IAdx4O7PtixWKn5y2hMNG0zQPyUecp4pzC6kivAIhyfHilFR61RGL+GPXQ2MWZWFYbAGjyiYJnAmCP3NOTd0jMZEnDkbUvxhMmBYSdETk1rRgm+R4LOzFUGaHqHDLKLX+FIPKcF96hrucXzcWyLbIbEgE98OHlnVYCzRdK8jlqm8tehUc9c9WhQ== vagrant insecure public key"
-ROOTFS=/var/lib/lxc/${RELEASE}-base/rootfs
+ROOTFS=/var/lib/lxc/${RELEASE}-base/${RELEASE}-base/rootfs
 
 # Providing '1' will enable these tools
 CHEF=${CHEF:-0}
