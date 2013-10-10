6d57b137144b0005f2bdc9663c14cb062128a900
Vokhmin Alexey V <avokhmin@gmail.com>
2013-10-10 15:30:50 +0400

    misprint

--- boxes/build-openmandriva-box.sh
+++ boxes/build-openmandriva-box.sh
@@ -87,7 +87,7 @@ chroot ${ROOTFS} usermod -a -G wheel vagrant
 
 # Enable passwordless sudo for users under the "sudo" group
 cp ${ROOTFS}/etc/sudoers{,.orig}
-sed -i 's/\#%wheel/\%wheel/' /etc/sudoers
+sed -i 's/\#%wheel/\%wheel/' ${ROOTFS}/etc/sudoers
 # sed -i -e \
 #       's/%sudo\s\+ALL=(ALL\(:ALL\)\?)\s\+ALL/%sudo ALL=NOPASSWD:ALL/g' \
 #       ${ROOTFS}/etc/sudoers
